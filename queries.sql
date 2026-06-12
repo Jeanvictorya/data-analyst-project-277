f7f95aa7957dc64a7ed087c7cc17056ac999fb21
Jean_Garcia <98372407+Jeanvictorya@users.noreply.github.com>
2026-06-12 15:36:07 -0500

    Add comment to SQL query for clarity
    Added a comment explaining the SQL query functionality.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 select 
 		COUNT(customer_id) as customers_count
 from customers;
+
+--El codigo selecciona todas las filas de la columna customer_id, postriormente las cuenta con la funcion COUNT y por ultimo le asigna un nombre temporal con el alias customer_count, todo esto se seleeciona desde la tabla customers 
